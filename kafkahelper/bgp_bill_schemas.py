4b879969865ff7fb666b8489466ce17cafafd49b
Patrick Sattler <sattler@net.in.tum.de>
2020-12-01 15:45:00 +0100

    add irr schema

--- kafkahelper/bgp_bill_schemas.py
+++ kafkahelper/bgp_bill_schemas.py
@@ -98,5 +98,106 @@ TLSTARGETSCHEMA = fastavro.parse_schema({
     ],
 })
 
-__all__ = ['BASICALERTSCHEMA', 'TLSTARGETSCHEMA']
+IRRSCHEMA = fastavro.parse_schema({
+  "name": "irrrov",
+  "type": "record",
+  "namespace": "heap",
+  "fields": [
+    {
+      "name": "id",
+      "type": "string"
+    },
+    {
+      "name": "rpki",
+      "type": "int"
+    },
+    {
+        "name": "prefix",
+        "namespace": "heap",
+        "type": {
+            "type": "record",
+            "name": "prefix",
+            "namespace": "heap",
+            "fields": [
+                {"name": "irr", "type": "boolean"},
+                {"name": "crank",
+                 "type": ["null", {
+                     "name": "crank",
+                     "type": "record",
+                     "fields": [
+                         {"name": "rank", "type": ["null", "int"]},
+                         {"name": "seen", "type": ["null", "boolean"]},
+                         {"name": "clique", "type": ["null", "boolean"]},
+                         {"name": "cc", "type": ["null", "string"]},
+                         {"name": "cone",
+                          "type": {
+                              "name": "cone",
+                              "type": "record",
+                              "fields": [
+                                  {"name": "numberAsns", "type": ["null", "int"]},
+                                  {"name": "numberPrefixes", "type": ["null", "int"]},
+                                  {"name": "numberAddresses", "type": ["null", "long"]}
+                              ]
+                          }
+                          },
+                         {"name": "asnDegree",
+                          "type": {
+                              "name": "asnDegree",
+                              "type": "record",
+                              "fields": [
+                                  {"name": "provider", "type": ["null", "int"]},
+                                  {"name": "peer", "type": ["null", "int"]},
+                                  {"name": "customer", "type": ["null", "int"]},
+                                  {"name": "total", "type": ["null", "int"]},
+                                  {"name": "transit", "type": ["null", "int"]},
+                                  {"name": "sibling", "type": ["null", "int"]}
+                              ]
+                          }
+                          },
+                         {"name": "announcing",
+                          "type": {
+                              "name": "announcing",
+                              "type": "record",
+                              "fields": [
+                                  {"name": "numberPrefixes", "type": ["null", "int"]},
+                                  {"name": "numberAddresses","type": ["null", "int"]}
+                              ]
+                          }
+                          }
+                     ]
+                 }]
+                 },
+                {"name": "aspop",
+                 "type":  ["null", {
+                     "name": "aspop",
+                     "type": "record",
+                     "fields": [
+                         {"name": "rank", "type": ["null", "int"]},
+                         {"name": "cc", "type": ["null", "string"]},
+                         {"name": "users", "type": ["null", "int"]},
+                         {"name": "samples", "type": ["null", "int"]}
+                     ]
+                 }]
+                 },
+                {"name": "cclass",
+                 "type":  ["null", {
+                     "name": "cclass",
+                     "type": "record",
+                     "fields": [
+                         {"name": "type", "type": ["null", "int"]},
+                         {"name": "reason", "type": ["null", "int"]}
+                     ]
+                 }]
+                 }
+            ]
+        }
+    },
+    {
+      "name": "subprefix",
+      "type": "prefix"
+    }
+  ]
+})
+
+__all__ = ['BASICALERTSCHEMA', 'TLSTARGETSCHEMA', 'IRRSCHEMA']
 
